67d6c2eeafa348cd0f0c2a10b22cd9e72e3a6ea6
peichincc <104899687+peichincc@users.noreply.github.com>
2022-08-31 20:46:01 +0800

    build the input form to user

--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -26,12 +26,12 @@ const Profile = () => {
   const [imageUpload, setImageUpload] = useState(null);
   const [imageURL, setImageURL] = useState("");
   const db = getFirestore();
-  const readData = async () => {
-    const querySnapshot = await getDocs(collection(db, "Users"));
-    querySnapshot.forEach((doc) => {
-      console.table(doc.data());
-    });
-  };
+  // const readData = async () => {
+  //   const querySnapshot = await getDocs(collection(db, "Users"));
+  //   querySnapshot.forEach((doc) => {
+  //     console.table(doc.data());
+  //   });
+  // };
 
   useEffect(() => {
     const userId = window.localStorage.getItem("userId");
@@ -50,13 +50,50 @@ const Profile = () => {
     setImageURL(downloadUrl);
   };
 
-  // 使用者寫入資料
+  // 把使用者放進db
   const pushtodb = async () => {
     await setDoc(doc(db, "Users", `${getUser}`), {
       name: "penny",
     });
   };
+
   // 使用者更新資訊
+  type ListData = {
+    user_id: number;
+    last_name: string;
+    first_name: string;
+    age: number;
+    gender: string;
+    email: string;
+    githublink: string;
+    details: string;
+    inerested_gender: string;
+    // inerested_gender: [];
+    main_photo: string;
+    relationship: string;
+    wish_relationship: string;
+  };
+
+  const [isRadio, setIsRadio] = useState("");
+  const handleChange = (e: any) => {
+    console.log(e.currentTarget.value);
+    setIsRadio(e.currentTarget.value);
+  };
+
+  const [recipient, setRecipient] = useState<ListData>({
+    user_id: getUser,
+    last_name: "",
+    first_name: "",
+    age: 20,
+    gender: "",
+    email: "",
+    githublink: "",
+    details: "",
+    inerested_gender: "",
+    main_photo: "",
+    relationship: "",
+    wish_relationship: "",
+  });
 
   // 讀取使用者資料
 
@@ -65,6 +102,41 @@ const Profile = () => {
       <Wrapper>
         <button onClick={pushtodb}>Set user</button>
         <Block>
+          <div>
+            <p>first name</p>
+            <input></input>
+            <p>last name</p>
+            <input></input>
+            <p>age</p>
+            <input></input>
+            <p>gender</p>
+            <input type="radio" value="male" name="gender"></input>Male
+            <input type="radio" value="female" name="gender"></input>Female
+            {/* <input type="radio" value="nb" name="gender"></input>Non-binary
+            <input type="radio" value="trans" name="gender"></input>Transgender
+            <input type="radio" value="intersex" name="gender"></input>Intersex */}
+            <input type="radio" value="not_to_say" name="gender"></input>Prefer
+            not to say
+            <p>interested gender</p>
+            <input
+              type="radio"
+              value="male"
+              name="gender_interested"
+              onChange={handleChange}
+            ></input>
+            Male
+            <input
+              type="radio"
+              value="female"
+              name="gender_interested"
+              onChange={handleChange}
+            ></input>
+            Female
+            <p>email</p>
+            <input></input>
+            <p>details</p>
+            <textarea></textarea>
+          </div>
           <input
             type="file"
             onChange={(e: any) => {
@@ -72,7 +144,7 @@ const Profile = () => {
             }}
           ></input>
           <button onClick={uploadImage}>Upload</button>
-          {imageURL && <img src={imageURL} alt="photo" />}
+          {imageURL && <img src={imageURL} alt="profile" />}
         </Block>
       </Wrapper>
     </>
