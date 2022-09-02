1b2e5508e80ad14272eeabca234b029a700e7b94
peichincc <104899687+peichincc@users.noreply.github.com>
2022-09-02 15:23:00 +0800

    Build up the createPost and send data to DB

--- src/pages/issue/CreateIssue.tsx
+++ src/pages/issue/CreateIssue.tsx
@@ -1,6 +1,22 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 
+import {
+  getFirestore,
+  doc,
+  setDoc,
+  updateDoc,
+  serverTimestamp,
+  collection,
+} from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+import {
+  ref,
+  uploadBytes,
+  getDownloadURL,
+  uploadBytesResumable,
+} from "firebase/storage";
+
 const Wrapper = styled.div`
   display: block;
   max-width: 1376px;
@@ -13,6 +29,10 @@ interface Data {
 }
 
 const CreateIssue = () => {
+  const db = getFirestore();
+  const storage = getStorage();
+  const [imageUpload, setImageUpload] = useState<any>(null);
+  const [fileSrc, setFileSrc] = useState<any>(null);
   const [checked, setChecked] = useState(false);
   const MyCheckBoxList: Data[] = [
     {
@@ -36,24 +56,101 @@ const CreateIssue = () => {
       name: "JavaScript",
     },
   ];
+  const handleUploadFile = (e: any) => {
+    if (!e.target.files[0]) return;
+    var reader = new FileReader();
+    reader.onload = function () {
+      setFileSrc(reader.result);
+    };
+    reader?.readAsDataURL(e?.target?.files[0]);
+    // e.target.value = "";
+    // setRecipientImage({ ...recipientImage, main_image: e.target.files });
+    setImageUpload(e.target.files[0]);
+  };
+
+  const [category, setCategory] = useState("");
+  const getCategory = (e: any) => {
+    setCategory(e.target.value);
+  };
+  const [title, setTitle] = useState("");
+  const getTitle = (e: any) => {
+    setTitle(e.target.value);
+  };
+  const [content, setContent] = useState("");
+  const getContent = (e: any) => {
+    setContent(e.target.value);
+  };
+  const [tags, setTags] = useState<any>([]);
+  const getTags = (e: any) => {
+    // setTags([...tags, e.target.value]);
+    if (e.target.checked) {
+      setTags([...tags, e.target.value]);
+    } else {
+      setTags(
+        tags.filter(function (val: any) {
+          return val !== e.target.value;
+        })
+      );
+    }
+  };
+
+  const [getUser, setGetUser] = useState<any>("");
+  useEffect(() => {
+    const userId = window.localStorage.getItem("userId");
+    console.log(userId);
+    if (userId) setGetUser(userId);
+  }, []);
+
+  // get id before setDoc
+  const newIssueRef = doc(collection(db, "Issues"));
+
+  const recipient = {
+    category: category,
+    title: title,
+    content: content,
+    tags: tags,
+    // main_image: downloadUrl,
+    status: "open",
+    posted_by: getUser,
+    posted_at: serverTimestamp(),
+  };
+
+  // upload photo w/ doc id, get photo URL, then setDoc
+  const postIssue = async () => {
+    const imageRef = ref(storage, `issues/${newIssueRef.id}.jpg`);
+    await uploadBytes(imageRef, imageUpload)
+      .then(() => {
+        alert("uploaded!");
+      })
+      .then(() => {
+        setDoc(newIssueRef, recipient);
+      })
+      .then(async () => {
+        const downloadUrl = await getDownloadURL(imageRef);
+        updateDoc(newIssueRef, {
+          issue_id: newIssueRef.id,
+          main_image: downloadUrl,
+        });
+      });
+  };
 
   return (
     <>
       <Wrapper>
         <h1>To create an issue</h1>
         <p>Category</p>
-        <select>
-          <option>Please Select your issue type</option>
-          <option>Date</option>
-          <option>Hang out</option>
-          <option>Networking</option>
+        <select onChange={getCategory}>
+          <option value="0">Please Select your issue type</option>
+          <option value="date">Date</option>
+          <option value="hangout">Hang out</option>
+          <option value="networking">Networking</option>
         </select>
         <br />
         <p>Title</p>
-        <input></input>
+        <input onChange={getTitle}></input>
         <br />
         <p>Content</p>
-        <textarea></textarea>
+        <textarea onChange={getContent}></textarea>
         <br />
         <p>Tags</p>
         <ul>
@@ -64,6 +161,7 @@ const CreateIssue = () => {
                   type="checkbox"
                   id={`custom-checkbox-${index}`}
                   value={name}
+                  onChange={getTags}
                 />
                 {name}
               </li>
@@ -71,7 +169,12 @@ const CreateIssue = () => {
           })}
         </ul>
         <br />
-        <h2>Upload image</h2>
+        <h2>Upload Main image</h2>
+        <input type="file" onChange={handleUploadFile}></input>
+        <p>Preview photo:</p>
+        {fileSrc && <img src={fileSrc} alt="main_image" />}
+        <h2>More images: pending</h2>
+        <button onClick={postIssue}>Post</button>
       </Wrapper>
     </>
   );
