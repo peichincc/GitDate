8729fe4ca59abba9cef32782083e80b9ef4b03c6
peichincc <104899687+peichincc@users.noreply.github.com>
2022-09-09 16:28:34 +0800

    The places search feature w/ Google API map

--- src/components/map/index.tsx
+++ src/components/map/index.tsx
@@ -1,9 +1,10 @@
-import React, { useMemo } from "react";
+import React, { useState, useMemo } from "react";
 import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
 import usePlacesAutocomplete, {
   getGeocode,
   getLatLng,
 } from "use-places-autocomplete";
+import useOnclickOutside from "react-cool-onclickoutside";
 
 import "./map.css";
 
@@ -19,12 +20,111 @@ const MapHome = () => {
 
 const Map = () => {
   const center = useMemo(() => ({ lat: 25.0384803, lng: 121.5301824 }), []);
+  const [selected, setSelected] = useState(null);
 
   return (
-    <GoogleMap zoom={10} center={center} mapContainerClassName="map-container">
-      <Marker position={center} />
-    </GoogleMap>
+    <>
+      <div className="places-container">
+        <PlacesAutocomplete setSelected={setSelected} />
+      </div>
+      <GoogleMap
+        zoom={10}
+        center={center}
+        mapContainerClassName="map-container"
+      >
+        {selected && <Marker position={selected} />}
+      </GoogleMap>
+    </>
   );
+};
+
+const PlacesAutocomplete = ({ setSelected }: any) => {
+  const {
+    ready,
+    value,
+    setValue,
+    suggestions: { status, data },
+    clearSuggestions,
+  } = usePlacesAutocomplete();
+
+  const ref = useOnclickOutside(() => {
+    // When user clicks outside of the component, we can dismiss
+    // the searched suggestions by calling this method
+    clearSuggestions();
+  });
+
+  const handleInput = (e: any) => {
+    // Update the keyword of the input element
+    setValue(e.target.value);
+  };
+  const handleSelect =
+    ({ description }: any) =>
+    () => {
+      // When user selects a place, we can replace the keyword without request data from API
+      // by setting the second parameter to "false"
+      setValue(description, false);
+      clearSuggestions();
+
+      // Get latitude and longitude via utility functions
+      getGeocode({ address: description }).then((results) => {
+        const { lat, lng } = getLatLng(results[0]);
+        console.log("📍 Coordinates: ", { lat, lng });
+        setSelected({ lat, lng });
+      });
+    };
+
+  const renderSuggestions = () =>
+    data.map((suggestion) => {
+      const {
+        place_id,
+        structured_formatting: { main_text, secondary_text },
+      } = suggestion;
+
+      return (
+        <li key={place_id} onClick={handleSelect(suggestion)}>
+          <strong>{main_text}</strong> <small>{secondary_text}</small>
+        </li>
+      );
+    });
+
+  return (
+    <div ref={ref}>
+      <input
+        value={value}
+        onChange={handleInput}
+        disabled={!ready}
+        placeholder="Where are you going?"
+      />
+      {/* We can use the "status" to decide whether we should display the dropdown or not */}
+      {status === "OK" && <ul>{renderSuggestions()}</ul>}
+    </div>
+  );
+  // const handleSelect = async (address?: string | null | undefined) => {
+  //   setValue(address, false);
+  //   clearSuggestions();
+
+  //   const results = await getGeocode({ address });
+  //   const { lat, lng } = await getLatLng(results[0]);
+  //   setSelected({ lat, lng });
+  // };
+
+  // return (
+  //   <>
+  //     <div className="input-container" onSelect={handleSelect}>
+  //       <input
+  //         className="input-container"
+  //         placeholder="Search an address"
+  //         value={value}
+  //         onChange={(e) => setValue(e.target.value)}
+  //         disabled={!ready}
+  //       />
+  //       <div>
+  //         {status === "OK" &&
+  //           data.map((place_id, description) => <option value={description} />)}
+  //       </div>
+  //     </div>
+  //   </>
+  // );
 };
 
 export default MapHome;
