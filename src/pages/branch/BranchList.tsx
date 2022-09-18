b33ff6edd8f4d95096580e434f3f9d1640551834
peichincc <104899687+peichincc@users.noreply.github.com>
2022-09-18 08:59:41 +0800

    Branch lists layout and set filter to branch type

--- src/pages/branch/BranchList.tsx
+++ src/pages/branch/BranchList.tsx
@@ -1,22 +1,82 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
-const BranchesList = ({ docs }: any) => {
+import {
+  BoxHeader,
+  Container,
+  ContentContainer,
+  BlogList,
+  GithubPostTitle,
+  GithubSubTitle,
+  Button,
+} from "../../utils/StyledComponent";
+
+import branch from "./branch.png";
+
+const BranchesHeader = styled(BoxHeader)`
+  font-size: 14px;
+  line-height: 1.5;
+  color: #24292f;
+  font-weight: 600;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+const BranchDate = styled.div`
+  font-size: 0.875rem;
+  font-weight: 500;
+  padding-bottom: 0.25rem;
+  padding-top: 0.25rem;
+  color: #877457;
+`;
+const GithubTitleContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: left;
+  padding: 8px;
+`;
+const LeftContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+const RightContainer = styled.div``;
+
+const BranchesList = ({ branchType, docs }: any) => {
   let navigate = useNavigate();
   return (
     <>
-      {docs.map((blog: any) => (
-        <>
-          <h2>Branch title: {blog.title}</h2>
-          <button
-            onClick={() => {
-              navigate("/branch/" + blog.branch_id);
-            }}
-          >
-            Click to Branch
-          </button>
-        </>
-      ))}
+      <Container>
+        <BranchesHeader>{branchType} Branches</BranchesHeader>
+        <ContentContainer>
+          {docs.map((blog: any) => (
+            <>
+              <BlogList>
+                <LeftContainer>
+                  <img
+                    src={branch}
+                    style={{ width: "20px", height: "20px" }}
+                    alt="branch_icon"
+                  />
+                  <GithubTitleContainer>
+                    <BranchDate>{blog.date}</BranchDate>
+                    <GithubPostTitle>{blog.title}</GithubPostTitle>
+                  </GithubTitleContainer>
+                </LeftContainer>
+                <RightContainer>
+                  <Button
+                    onClick={() => {
+                      navigate("/branch/" + blog.branch_id);
+                    }}
+                  >
+                    Click to Branch
+                  </Button>
+                </RightContainer>
+              </BlogList>
+            </>
+          ))}
+        </ContentContainer>
+      </Container>
     </>
   );
 };
--- src/pages/branch/BranchesAll.tsx
+++ src/pages/branch/BranchesAll.tsx
@@ -15,6 +15,8 @@ import BranchesList from "./BranchList";
 
 import firebaseapi from "../../utils/firebaseapi";
 
+import { MergeBtn, Button, LabelsButton } from "../../utils/StyledComponent";
+
 const Wrapper = styled.div`
   display: flex;
   max-width: 1376px;
@@ -26,24 +28,117 @@ const CalendarContainer = styled.div`
   margin-right: 20px;
 `;
 const BranchesContainer = styled.div`
+  margin-top: 20px;
   width: 60%;
+`;
+
+const FilterContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+const Filters = styled.div`
+  width: 100%;
+  margin-right: 10px;
+  height: 35px;
+  border-radius: 6px;
+  background-color: #f6f8fa;
+  border-color: #d0d7de;
+  border-style: solid;
+  border-width: 1px;
+  display: flex;
+  align-items: center;
+  @media screen and (max-width: 661px) {
+    height: auto;
+  }
+`;
+const FilterText = styled.div`
+  padding: 5px 16px;
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 20px;
+  color: #24292f;
+  border-right: 1px solid #d0d7de;
+`;
+const FilterButtons = styled.div`
+  margin-left: 5px;
+`;
+
+const TypeBtn = styled(LabelsButton)`
+  background-color: #453d38;
 `;
 
 const BranchAll = () => {
   const [date, setDate] = useState(new Date());
   let navigate = useNavigate();
   const db = getFirestore();
-  const [docs, setDocs] = useState([]);
+  const [docs, setDocs] = useState<DocumentData>();
   const [dateDocs, setDateDocs] = useState<DocumentData>();
+  const [allBranch, setAllbranch] = useState<DocumentData>();
+  const [branchType, setBranchType] = useState("");
+  const [inpersonBranch, setInpersonBranch] = useState<DocumentData>();
+  const [onlineBranch, setOnlineBranch] = useState<DocumentData>();
+  const [mixedBranch, setMixedBranch] = useState<DocumentData>();
 
   useEffect(() => {
     const branchesRef = collection(db, "Branches");
-    firebaseapi.readAllBranches(branchesRef).then((res) => {
+    firebaseapi.readAllBranches(branchesRef).then(async (res) => {
       if (res) {
         setDocs(res);
+        setAllbranch(res);
+        setBranchType("All");
+        // get inperson
+        let temp = [] as any;
+        const q = query(
+          collection(db, "Branches"),
+          where("type", "==", "inperson")
+        );
+        const querySnapshot = await getDocs(q);
+        querySnapshot.forEach((doc) => {
+          temp.push(doc.data());
+        });
+        setInpersonBranch(temp);
+        // get online
+        let tempOnline = [] as any;
+        const qOnline = query(
+          collection(db, "Branches"),
+          where("type", "==", "online")
+        );
+        const querySnapshotOnline = await getDocs(qOnline);
+        querySnapshotOnline.forEach((doc) => {
+          tempOnline.push(doc.data());
+        });
+        setOnlineBranch(tempOnline);
+        // get mixed
+        let tempMixed = [] as any;
+        const qMixed = query(
+          collection(db, "Branches"),
+          where("type", "==", "mixed")
+        );
+        const querySnapshotMixed = await getDocs(qMixed);
+        querySnapshotMixed.forEach((doc) => {
+          tempMixed.push(doc.data());
+        });
+        setMixedBranch(tempMixed);
       }
     });
   }, []);
+
+  const allBranches = () => {
+    setBranchType("All");
+    setDocs(allBranch);
+  };
+  const inpersonBranches = () => {
+    setBranchType("Inperson");
+    setDocs(inpersonBranch);
+  };
+  const onlineBranches = () => {
+    setBranchType("Online");
+    setDocs(onlineBranch);
+  };
+  const mixedBranches = () => {
+    setBranchType("Mixed");
+    setDocs(mixedBranch);
+  };
 
   const dateClick = async (date: any) => {
     // console.log(date);
@@ -86,10 +181,26 @@ const BranchAll = () => {
             <span className="bold">Selected Date:</span> {date.toDateString()}
           </p>
         </CalendarContainer>
-
         <BranchesContainer>
-          <h1>Display all branches here.</h1>
-          {docs && <BranchesList docs={docs} />}
+          <FilterContainer>
+            <Filters>
+              <FilterText>Filters</FilterText>
+              <FilterButtons>
+                <TypeBtn onClick={allBranches}>All</TypeBtn>
+                <TypeBtn onClick={inpersonBranches}>In Person</TypeBtn>
+                <TypeBtn onClick={onlineBranches}>Online</TypeBtn>
+                <TypeBtn onClick={mixedBranches}>Mixed</TypeBtn>
+              </FilterButtons>
+            </Filters>
+            <MergeBtn
+              onClick={() => {
+                navigate("/createbranch");
+              }}
+            >
+              New branch
+            </MergeBtn>
+          </FilterContainer>
+          {docs && <BranchesList docs={docs} branchType={branchType} />}
         </BranchesContainer>
       </Wrapper>
     </>
