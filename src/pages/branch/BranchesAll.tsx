f93c474b0dd032fea1c001bca307cc6c5402d112
peichincc <104899687+peichincc@users.noreply.github.com>
2022-09-18 10:29:36 +0800

    Edit calendar styling

--- src/pages/branch/BranchesAll.tsx
+++ src/pages/branch/BranchesAll.tsx
@@ -10,12 +10,85 @@ import {
 } from "firebase/firestore";
 import styled from "styled-components";
 import Calendar from "react-calendar";
-import "./calendar.css";
+// import "./calendar.css";
 import BranchesList from "./BranchList";
 
 import firebaseapi from "../../utils/firebaseapi";
 
 import { MergeBtn, Button, LabelsButton } from "../../utils/StyledComponent";
+
+const CalendarContainer = styled.div`
+  /* ~~~ container styles ~~~ */
+  max-width: 600px;
+  margin: auto;
+  margin-top: 20px;
+  /* background-color: #d4f7d4; */
+  padding: 10px;
+  border-radius: 3px;
+  /* ~~~ navigation styles ~~~ */
+  .react-calendar__navigation {
+    display: flex;
+    .react-calendar__navigation__label {
+      font-weight: bold;
+    }
+    .react-calendar__navigation__arrow {
+      flex-grow: 0.333;
+    }
+  }
+  /* ~~~ label styles ~~~ */
+  .react-calendar__month-view__weekdays {
+    text-align: center;
+  }
+  /* ~~~ button styles ~~~ */
+  button {
+    margin: 3px;
+    background: none;
+    /* background-color: #6f876f; */
+    border: 0;
+    border-radius: 3px;
+    color: black;
+    padding: 5px 0;
+    &:hover {
+      background-color: #ff69b4;
+    }
+    &:active {
+      background-color: #f6f8fa;
+    }
+  }
+  /* ~~~ day grid styles ~~~ */
+  .react-calendar__month-view__days {
+    display: grid !important;
+    grid-template-columns: 14.2% 14.2% 14.2% 14.2% 14.2% 14.2% 14.2%;
+
+    .react-calendar__tile {
+      max-width: initial !important;
+    }
+    .react-calendar__tile--range {
+      border: 1px solid;
+      /* box-shadow: 0 0 6px 2px black; */
+    }
+  }
+  /* ~~~ neighboring month & weekend styles ~~~ */
+  .react-calendar__month-view__days__day--neighboringMonth {
+    opacity: 0.7;
+  }
+  .react-calendar__month-view__days__day--weekend {
+    color: #dfdfdf;
+  }
+  /* ~~~ other view styles ~~~ */
+  .react-calendar__year-view__months,
+  .react-calendar__decade-view__years,
+  .react-calendar__century-view__decades {
+    display: grid !important;
+    grid-template-columns: 20% 20% 20% 20% 20%;
+    &.react-calendar__year-view__months {
+      grid-template-columns: 33.3% 33.3% 33.3%;
+    }
+    .react-calendar__tile {
+      max-width: initial !important;
+    }
+  }
+`;
 
 const Wrapper = styled.div`
   display: flex;
@@ -36,10 +109,10 @@ const Container = styled.div`
     align-items: center;
   }
 `;
-const CalendarContainer = styled.div`
-  width: 30%;
-  margin-right: 20px;
-`;
+// const CalendarContainer = styled.div`
+//   width: 30%;
+//   margin-right: 20px;
+// `;
 const CalendarContainerIn = styled.div`
   border: 1px solid #d0d7de;
   border-radius: 6px;
